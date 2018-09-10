de4beb625f2e1280f982209d6565b60299d533e2
wudianbo <dianbowu@jieyuechina.com>
2018-09-10 20:21:39 +0800

    Merge branch 'master' of D:\abb\CRM- with conflicts.

--- src/main/java/com/crm/crmsystem/H5Controller/HtmlController.java
+++ src/main/java/com/crm/crmsystem/H5Controller/HtmlController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -59,5 +60,15 @@ public class HtmlController {
     ResponseData<String> listCustomer = customerController.selAllCus(pageForm);
     request.setAttribute("listCustomer", listCustomer);
     return "customer";
+  }
+  @PostMapping("customerPage")
+  @ResponseBody
+  public ResponseData<String> customer(int pageIndex) {
+    PageForm<Customer> pageForm = new PageForm();
+    Customer customer=new Customer();
+    pageForm.setForm(customer);
+    pageForm.setPageIndex(pageIndex);
+
+    return customerController.selAllCus(pageForm);
   }
 }
